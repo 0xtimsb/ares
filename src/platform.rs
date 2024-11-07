2b1c24089f67b472f8c9302efa8deae15cc6b775
0xtimsb <0xtimsb@gmail.com>
2024-11-07 17:11:39 +0530

    add lot of comments for learning purposes

--- src/platform.rs
+++ src/platform.rs
@@ -5,6 +5,7 @@ mod linux;
 pub(crate) use linux::*;
 
 pub(crate) trait Platform: 'static {
+    // Box is used here cause FnOnce is a trait object and needs to be sized.
     fn run(&self, on_finish_launching: Box<dyn 'static + FnOnce()>);
     fn quit(&self);
 }
--- src/platform/linux/x11/client.rs
+++ src/platform/linux/x11/client.rs
@@ -12,6 +12,7 @@ pub struct X11ClientState {
 pub(crate) struct X11Client(Rc<RefCell<X11ClientState>>);
 
 impl X11Client {
+    // returns a new instance of X11Client
     pub(crate) fn new() -> Self {
         let (conn, screen_num) = XCBConnection::connect(None).unwrap();
 
@@ -22,5 +23,6 @@ impl X11Client {
 }
 
 impl LinuxClient for X11Client {
+    // x11 specific logic to start the app
     fn run(&self) {}
 }
